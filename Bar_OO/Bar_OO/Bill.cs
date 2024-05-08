f4d5ffaff6a053d26a2b9286cd7b2cd940dc2d7a
sidineischeffer <61559194+sidineischeffer@users.noreply.github.com>
2024-05-08 13:57:22 -0300

    Add files via upload

---
+++ Bar_OO/Bar_OO/Bill.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bar_OO {
+    class Bill {
+        public char gender;
+        public int beer;
+        public int barbecue;
+        public int softDrink;
+
+        private static double precoBeer = 5.00;
+        private static double precoSoftDrink = 3.00;
+        private static double precoBarbecue = 7.00;
+        private static double coverPagar = 4.00;
+        private static double ticketMasculino = 10.00;
+        private static double ticketFeminino = 8.00;
+
+        public double ticket() {
+            if (gender == 'F') {
+                return ticketFeminino;
+            }
+            else {
+                return ticketMasculino;
+            }
+        }
+
+        public double cover(double totalConsumption) {
+            return totalConsumption > 30.00 ? 0.00 : coverPagar;
+        }
+
+        public double feeding() {
+            return (precoBeer * beer) + (precoSoftDrink * softDrink) + (precoBarbecue * barbecue);
+        }
+
+        public double total() {
+            double totalConsumption = feeding();
+            double totalCover = cover(totalConsumption);
+            return totalConsumption + ticket() + totalCover;
+        }
+    }
+}
---
+++ Bar_OO/Bar_OO/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Bar_OO {
+    internal class Program {
+        static void Main(string[] args) {
+            Bill conta = new Bill();
+
+            Console.Write("Informe o sexo (F ou M): ");
+            conta.gender = char.ToUpper(Console.ReadKey().KeyChar);
+            Console.ReadLine();
+            Console.WriteLine($"Sexo registrado: {conta.gender}");
+
+            Console.Write("Informe a quantidade de cerveja: ");
+            conta.beer = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe a quantidade de Refrigerantes: ");
+            conta.softDrink = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe a quantidade de Espetinhos: ");
+            conta.barbecue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("-------------RELATORIO------------------");
+            Console.WriteLine($"Consumo: R$ {conta.feeding().ToString("F2", CultureInfo.InvariantCulture)}");
+            double totalCover = conta.cover(conta.feeding());
+            if (totalCover == 0) {
+                Console.WriteLine("Couvert: Isento de Couvert");
+            }
+            else {
+                Console.WriteLine($"Couvert: R$ {totalCover.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+
+            Console.WriteLine($"Ingresso: R$ {conta.ticket().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Total: R$ {conta.total().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
